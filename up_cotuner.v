76b87fb82607487bf7f0e53d25c6ebcc9b702ec3
haoweilu <haoweilu.go@gmail.com>
2025-11-27 22:21:54 +0800

    first commit

---
+++ up_cotuner.v
@@ -0,0 +1,16 @@
+module up_counter (
+    input wire clk,
+    input wire reset,
+    output reg [3:0] count
+);
+
+    always @(posedge clk or posedge reset) begin
+        if (reset) begin
+            count <= 4'b0000;
+        end else begin
+            count <= count + 1;
+        end
+    end
+);
+    
+endmodule
